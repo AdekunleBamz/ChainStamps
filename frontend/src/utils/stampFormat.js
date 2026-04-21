0a4be0a2db46b071793b3c966ff35fcffd30198f
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 19:18:15 +0100

    feat: add formatStampId helper

--- frontend/src/utils/stampFormat.js
+++ frontend/src/utils/stampFormat.js
@@ -4,3 +4,5 @@ export const formatStampHash = (h) => h.slice(0, 8) + "..." + h.slice(-8);
 export const formatStampFee = (v) => (v / 1e6).toFixed(6) + " STX";
 
 export const formatStampDate = (ts) => new Date(ts).toLocaleDateString();
+
+export const formatStampId = (id) => "STAMP-" + id;
