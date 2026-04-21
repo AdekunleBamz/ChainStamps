0bd004d8500036d61446fe94da388ef055efd792
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 20:19:50 +0100

    feat: add truncateAddress helper

--- frontend/src/utils/stampHelpers.js
+++ frontend/src/utils/stampHelpers.js
@@ -6,3 +6,5 @@ export const parseStampResponse = (res) => res && res.txid ? res : null;
 export const isStampExpired = (expiry, current) => current > expiry;
 
 export const getStampAge = (blockStamped, currentBlock) => currentBlock - blockStamped;
+
+export const truncateAddress = (addr) => addr ? addr.slice(0, 8) + "..." + addr.slice(-6) : "";
