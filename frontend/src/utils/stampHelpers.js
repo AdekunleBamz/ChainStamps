8e5bb7ff6159a1ea4a6862460866b373a3faf8e2
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 20:19:50 +0100

    feat: add getStampAge helper

--- frontend/src/utils/stampHelpers.js
+++ frontend/src/utils/stampHelpers.js
@@ -4,3 +4,5 @@ export const buildStampPayload = (hash, memo) => ({ hash, memo });
 export const parseStampResponse = (res) => res && res.txid ? res : null;
 
 export const isStampExpired = (expiry, current) => current > expiry;
+
+export const getStampAge = (blockStamped, currentBlock) => currentBlock - blockStamped;
