645376eecd0ce84d6afa96e79b9a2fbca0cdf28a
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 20:19:50 +0100

    feat: add stampUrl helper

--- frontend/src/utils/stampHelpers.js
+++ frontend/src/utils/stampHelpers.js
@@ -8,3 +8,5 @@ export const isStampExpired = (expiry, current) => current > expiry;
 export const getStampAge = (blockStamped, currentBlock) => currentBlock - blockStamped;
 
 export const truncateAddress = (addr) => addr ? addr.slice(0, 8) + "..." + addr.slice(-6) : "";
+
+export const stampUrl = (txid, network) => network === "mainnet" ? "https://explorer.hiro.so/txid/" + txid : "https://explorer.hiro.so/txid/" + txid + "?chain=testnet";
