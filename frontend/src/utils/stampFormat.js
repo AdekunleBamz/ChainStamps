ad277699a7c238065be1a94b864ff239f2c99d13
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 19:18:15 +0100

    feat: add formatBlockHeight helper

--- frontend/src/utils/stampFormat.js
+++ frontend/src/utils/stampFormat.js
@@ -6,3 +6,5 @@ export const formatStampFee = (v) => (v / 1e6).toFixed(6) + " STX";
 export const formatStampDate = (ts) => new Date(ts).toLocaleDateString();
 
 export const formatStampId = (id) => "STAMP-" + id;
+
+export const formatBlockHeight = (n) => "Block #" + n;
