d3111064500e9e8ebb1b3b40bd722803396e3a7b
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 19:18:16 +0100

    feat: add formatStampCount helper

--- frontend/src/utils/stampFormat.js
+++ frontend/src/utils/stampFormat.js
@@ -14,3 +14,5 @@ export const formatStampStatus = (s) => s.charAt(0).toUpperCase() + s.slice(1);
 export const formatTxId = (id) => id.slice(0, 8) + "...";
 
 export const formatMicroStx = (v) => (v / 1e6).toFixed(6) + " STX";
+
+export const formatStampCount = (n) => n + " stamps";
