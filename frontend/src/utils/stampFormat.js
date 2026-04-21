1c9218a51ba29321bc00f3cc41a7b6a04b25e8e7
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 19:18:17 +0100

    feat: add formatProofLength helper

--- frontend/src/utils/stampFormat.js
+++ frontend/src/utils/stampFormat.js
@@ -22,3 +22,5 @@ export const formatBatchId = (id) => "BATCH-" + id;
 export const formatFileSize = (b) => (b / 1024).toFixed(2) + " KB";
 
 export const formatWalletAddress = (a) => a.slice(0, 6) + "..." + a.slice(-4);
+
+export const formatProofLength = (n) => n + " chars";
