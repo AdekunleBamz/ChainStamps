b315a21c2e1e406238c84f3c4e9f379e161ce12d
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:53:44 +0100

    feat: add STACKS_API_BASE constant

--- frontend/src/utils/stampConstants.js
+++ frontend/src/utils/stampConstants.js
@@ -8,3 +8,5 @@ export const MIN_STAMP_FEE_USTX = 1000000
 export const MAX_STAMP_DATA_LENGTH = 256
 
 export const STAMP_VERSION = "1.0.0"
+
+export const STACKS_API_BASE = "https://stacks-node-api.mainnet.stacks.co"
