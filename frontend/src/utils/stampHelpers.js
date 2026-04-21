d73665924a57c01cb28ae4335285a33f05c50c1e
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 20:19:50 +0100

    feat: add isStampExpired helper

--- frontend/src/utils/stampHelpers.js
+++ frontend/src/utils/stampHelpers.js
@@ -2,3 +2,5 @@
 export const buildStampPayload = (hash, memo) => ({ hash, memo });
 
 export const parseStampResponse = (res) => res && res.txid ? res : null;
+
+export const isStampExpired = (expiry, current) => current > expiry;
