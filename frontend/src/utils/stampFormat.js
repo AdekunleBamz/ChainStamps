04c8f471eed60f8ad97fc136a814c981bf1f9027
AdekunleBamz <bams.kunle@gmail.com>
2026-04-26 08:25:19 +0100

    add formatStampFeeUstx micro-STX fee label

--- frontend/src/utils/stampFormat.js
+++ frontend/src/utils/stampFormat.js
@@ -71,3 +71,5 @@ export const formatStampAge = (blocks, blocksPerDay) => {
 };
 
 export const formatBatchProgress = (done, total) => done + " / " + total + " stamps";
+
+export const formatStampFeeUstx = (v) => Number(v).toLocaleString() + " µSTX fee";
