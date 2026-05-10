40b43a29dd6098d0fa7c4ba7a97e68d2e083c44c
AdekunleBamz <bams.kunle@gmail.com>
2026-05-10 06:59:49 +0100

    docs(stampHelpers): add JSDoc to isStampExpired

--- frontend/src/utils/stampHelpers.js
+++ frontend/src/utils/stampHelpers.js
@@ -9,6 +9,12 @@ export const buildStampPayload = (hash, memo) => ({ hash, memo });
 
 export const parseStampResponse = (res) => res && res.txid ? res : null;
 
+/**
+ * Returns true if the stamp has passed its expiry block.
+ * @param {number} expiry - Expiry block height
+ * @param {number} current - Current block height
+ * @returns {boolean}
+ */
 export const isStampExpired = (expiry, current) => current > expiry;
 
 export const getStampAge = (blockStamped, currentBlock) => currentBlock - blockStamped;
