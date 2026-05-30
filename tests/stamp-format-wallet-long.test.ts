535277d49faa3c9e0f33b29b879871a006557c30
AdekunleBamz <bams.kunle@gmail.com>
2026-05-30 20:25:58 +0100

    test: correct long wallet formatter expectation

--- tests/stamp-format-wallet-long.test.ts
+++ tests/stamp-format-wallet-long.test.ts
@@ -3,6 +3,6 @@ import { formatWalletAddress } from '../frontend/src/utils/stampFormat'
 
 describe('wallet formatter long address', () => {
   it('preserves the beginning and end of long wallet addresses', () => {
-    expect(formatWalletAddress('SP123456789ABCDEFG')).toBe('SP1234...CDEFG')
+    expect(formatWalletAddress('SP123456789ABCDEFG')).toBe('SP1234...DEFG')
   })
 })
